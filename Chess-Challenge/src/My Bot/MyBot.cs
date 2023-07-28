5f1e239146043c52dd7b362298cc509edc0e70e0
quartzar <joey.dorn7@gmail.com>
2023-07-28 22:10:32 +0100

    Added randomised move gen

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -5,6 +5,8 @@ public class MyBot : IChessBot
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        // generate random integer
+        int random = new System.Random().Next(0, moves.Length);
+        return moves[random];
     }
 }
