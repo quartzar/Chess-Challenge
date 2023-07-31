88f643d96a7c0689e43e4ac8934631db56884b1a
quartzar <joey.dorn7@gmail.com>
2023-07-31 12:28:09 +0100

    Update Settings.cs

--- Chess-Challenge/src/Framework/Application/Core/Settings.cs
+++ Chess-Challenge/src/Framework/Application/Core/Settings.cs
@@ -8,7 +8,7 @@ namespace ChessChallenge.Application
 
         // Game settings
         public const int GameDurationMilliseconds = 60 * 1000;
-        public const float MinMoveDelay = 0;
+        public const float MinMoveDelay = 0; //0.1f;
         public static readonly bool RunBotsOnSeparateThread = true;
 
         // Display settings
