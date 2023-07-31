993553c86ab99ade6c71314000b87d454e88d364
quartzar <joey.dorn7@gmail.com>
2023-07-31 21:11:17 +0100

    Add ability to select other opponents

--- Chess-Challenge/src/Framework/Application/UI/MenuUI.cs
+++ Chess-Challenge/src/Framework/Application/UI/MenuUI.cs
@@ -9,7 +9,7 @@ namespace ChessChallenge.Application
     {
         public static void DrawButtons(ChallengeController controller)
         {
-            Vector2 buttonPos = UIHelper.Scale(new Vector2(260, 210));
+            Vector2 buttonPos = UIHelper.Scale(new Vector2(150, 210));
             Vector2 buttonSize = UIHelper.Scale(new Vector2(260, 55));
             float spacing = buttonSize.Y * 1.2f;
             float breakSpacing = spacing * 0.6f;
@@ -76,6 +76,18 @@ namespace ChessChallenge.Application
                 pos.Y += spacingY;
                 return pressed;
             }
+
+            // Custom game buttons
+            buttonPos = UIHelper.Scale(new Vector2(405, 210)); 
+            buttonSize = UIHelper.Scale(new Vector2(200, 55)); 
+            
+            if (NextButtonInRow("MyBot vs V4", ref buttonPos, spacing, buttonSize)) { 
+                controller.StartNewBotMatch(ChallengeController.PlayerType.MyBot, ChallengeController.PlayerType.V4); 
+            }
+            if (NextButtonInRow("MyBot vs V5", ref buttonPos, spacing, buttonSize)) { 
+                controller.StartNewBotMatch(ChallengeController.PlayerType.MyBot, ChallengeController.PlayerType.V5); 
+            }
+
         }
     }
 }
