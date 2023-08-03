d7e8c0197794ffe853b9ee559e85e8f95ecfd8b5
quartzar <joey.dorn7@gmail.com>
2023-08-03 11:51:49 +0100

    TT, QSearch, improved move ordering
    - Approximate ELO increase of ~200 over V5.
    - Transposition table lookup along with improved MVV-LVA greatly reduces time complexity

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -7,7 +7,7 @@ using System.Linq;
 public class MyBot : IChessBot
 {
     // 0 = None, 1 = Pawn, 2 = Knight, 3 = Bishop, 4 = Rook, 5 = Queen, 6 = King
-    int[] pieceValues = { 0, 10, 30, 30, 50, 90, 900 };
+    int[] pieceValues = { 0, 10, 31, 33, 50, 90, 900};
 
     //--------------------------------------------------------------------------------
     // Transposition table                                            CREDIT: Selenaut
@@ -29,51 +29,43 @@ public class MyBot : IChessBot
     //To access => ref Transposition transposition = ref m_TPTable[board.ZobristKey & k_TpMask];
     //you also need to check to make sure that the stored hash is equal to the board state you're interested in, alongside your normal checks w.r.t. depth etc.
 
+    Move[] m_killerMoves;
+
+    Board m_board;
+
     public MyBot()
     {
+        m_killerMoves = new Move[1024];
         m_TPTable = new Transposition[0x800000];
     }
-
-    // Board m_board;
 
     //--------------------------------------------------------------------------------
 
     public Move Think(Board board, Timer timer)
     {
-        Move[] newGameMoves = board.GetLegalMoves();
-        // m_board = board;
-        
-        // Play a random move if nothing better is found
-        Random rng = new();
-        Move moveToPlay = newGameMoves[rng.Next(newGameMoves.Length)];
-        
-        // Depth of the minimax algorithm
-        int depth = 3;
-        int bestEvaluation = -9999;
+        m_board = board;
 
         int monkeyCounter = 0; //#DEBUG
         int ttCounter = 0; //#DEBUG
         int startTime = timer.MillisecondsElapsedThisTurn; //#DEBUG
 
+        Transposition moveToPlay = m_TPTable[board.ZobristKey & 0x7FFFFF];
+        int maxTime = timer.MillisecondsRemaining/50;
 
-        foreach (Move newGameMove in newGameMoves)
-        {   // Make the move 
-            board.MakeMove(newGameMove);
-
-            int moveValue = -Negamax(depth - 1, newGameMove, -10000, 10000);
-
-            board.UndoMove(newGameMove);
-
-            // Evaluate best move 
-            if (moveValue >= bestEvaluation) {
-                bestEvaluation = moveValue;
-                moveToPlay = newGameMove;
-            }
+        for (sbyte depth = 1;; depth++) // this is going to break // it did break
+        {
+            Negamax(depth, -100000000, 100000000);
+            moveToPlay = m_TPTable[board.ZobristKey & 0x7FFFFF];
+            Console.WriteLine($"Depth: {depth}"); //#DEBUG
+            if (!ShouldExecuteNextDepth(timer, maxTime)) break;
         }
+
+        
 
         // Log time taken to make a move
         // Console.WriteLine($"Move time: {timer.MillisecondsElapsedThisTurn}ms");
-        // Console.WriteLine($"Monkey counter: {monkeyCounter}");
+        string formattedMonkeyCounter = string.Format("{0:n0}", monkeyCounter); //#DEBUG
+        Console.WriteLine($"Monkey counter: {formattedMonkeyCounter}"); //#DEBUG
 
         // Log positions per second
         int moveTime = timer.MillisecondsElapsedThisTurn - startTime; //#DEBUG
@@ -81,87 +73,107 @@ public class MyBot : IChessBot
         = monkeyCounter > 0 & moveTime > 0 ? (int)(monkeyCounter * 1000 / moveTime) : 0; //#DEBUG
         string formattedPositionsPerSecond = string.Format("{0:n0}", positionsPerSecond); //#DEBUG
         Console.WriteLine($"Positions/s => {formattedPositionsPerSecond}"); //#DEBUG
-        // Console.WriteLine($"TTable hits => {ttCounter}"); //#DEBUG
+        Console.WriteLine($"TTable hits => {ttCounter}"); //#DEBUG
 
-        return moveToPlay;
+        return moveToPlay.move;
 
 
 //------//--------------------------------------------------------------------------------
 //------// Functions
 //------//--------------------------------------------------------------------------------
 
+        //--------------------------------------------------------------------------------
+
 
         // Negamax algorithm
         //--------------------------------------------------------------------------------
-        int Negamax(int depth, Move move, int alpha, int beta)
+        int Negamax(int depth, int alpha, int beta)
         {
-
-            Move[] newGameMoves = board.GetLegalMoves();
-
-            // Do not evaluate if move leads to draw or checkmate
-            if (board.IsDraw()) return 0; // ~75+/-20 Elo increase over V4
-            if (newGameMoves.Length == 0) // Checkmate
-            {
-                return board.PlyCount -99999;
-            }
+            bool pvNode = alpha + 1 < beta;
+            bool inQSearch = depth <= 0;
+            int startingAlpha = alpha;
+            int bestEvaluation = -2147483647;              
 
             // Transposition table lookup
-            // ref Transposition tp = ref m_TPTable[m_board.ZobristKey & 0x7FFFFF];
-            // if (tp.zobristHash == m_board.ZobristKey && tp.depth >= depth)
-            // {
-            //     ttCounter++; //#DEBUG
-            //     switch (tp.flag)
-            //     {
-            //         case 1: // EXACT
-            //             return tp.evaluation;
-            //         case 2: // LOWERBOUND
-            //             alpha = Math.Max(alpha, tp.evaluation);
-            //             break;
-            //         case 3: // UPPERBOUND
-            //             beta = Math.Min(beta, tp.evaluation);
-            //             break;
-            //     }
-            //     if (alpha >= beta) 
-            //         return tp.evaluation;
-            // }
+            ref Transposition TT = ref m_TPTable[m_board.ZobristKey & 0x7FFFFF];
+            if (TT.zobristHash == m_board.ZobristKey && TT.depth >= depth)
+            {
+                ttCounter++; //#DEBUG
+                // If score is exact (alpha < score < beta), return it
+                if (TT.flag == 1) return TT.evaluation;
+                // If lowerbound (score >= beta), return it
+                else if (TT.flag == 2 && TT.evaluation >= beta) return TT.evaluation;
+                // If upperbound (score <= alpha), return it
+                else if (TT.flag == 3 && TT.evaluation <= alpha) return TT.evaluation;
+            }
 
-            // If depth is 0, evaluate the board
-            if (depth == 0) 
-                return EvaluateBoard();
+            // Move[] newGameMoves = board.GetLegalMoves();
+
+            // Do not evaluate if move leads to draw or checkmate
+            if (m_board.IsDraw()) return -10; // ~75+/-20 Elo increase over V4
+            if (m_board.IsInCheckmate()) return m_board.PlyCount - 1000000; // checkmate
+
+            int standingPat = EvaluateBoard();
+
+
+            // Retrieve captures if in quiescence search AND not in check, 
+            // otherwise retrieve all legal moves
+            Move[] newMoves = board.GetLegalMoves(inQSearch && !m_board.IsInCheck());
+            if (newMoves.Length == 0) return standingPat; // stalemate (cannot reach in checkmate)
+            if (inQSearch)
+            {
+                if (standingPat >= beta) return standingPat;
+                if (standingPat > alpha) alpha = standingPat;
+            }
 
 
             // Order the moves for efficiency TODO: Improve ordering, reduce token count
-            Move[] orderedMoves = OrderMoves(newGameMoves);
+            OrderMoves(ref newMoves, depth);
+            Move bestMove = newMoves[0]; // assuming first move is best move
 
-            int bestEvaluation = -9999;          
-
-            foreach (Move orderedMove in orderedMoves)
-            {   
-                board.MakeMove(orderedMove);
                 
-                int moveValue = -Negamax(depth - 1, orderedMove, -beta, -alpha);  
-                bestEvaluation = Math.Max(bestEvaluation, moveValue);
 
-                board.UndoMove(orderedMove); 
+            for (int m = 0; m < newMoves.Length; m++)
+            {   
+                Move move = newMoves[m];
+                m_board.MakeMove(move);
+                
+                int moveValue = -Negamax(depth - 1, -beta, -alpha);
+                // int moveValue = -Negamax(depth - 1, (inQSearch || m == 0) ? -beta : -alpha - 1, -alpha);  
+                // if (!inQSearch && m != 0 && moveValue > alpha && moveValue < beta)
+                //     moveValue = -Negamax(depth - 1, -beta, -alpha); // re-search if null window was good
+                // bestEvaluation = Math.Max(bestEvaluation, moveValue);
+                m_board.UndoMove(move); 
+
+                if (bestEvaluation < moveValue)
+                {
+                    bestEvaluation = moveValue;
+                    bestMove = move;
+                }
 
                 // Alpha Beta pruning
                 alpha = Math.Max(alpha, bestEvaluation);
-                if (alpha >= beta) 
-                    break;
+                if (alpha >= beta) break;
             }
 
             // Store the evaluation of the current position into the transposition table
-            // tp.zobristHash = m_board.ZobristKey;
-            // tp.evaluation = bestEvaluation;
-            // tp.depth = (sbyte)depth;
-
-            // if (bestEvaluation <= alpha)
-            //     tp.flag = 3; // UPPERBOUND
-            // else if (bestEvaluation >= beta)
-            //     tp.flag = 2; // LOWERBOUND
-            // else
-            //     tp.flag = 1; // EXACT
-
+            if (!inQSearch)
+            {
+                TT.zobristHash = m_board.ZobristKey;
+                TT.evaluation = bestEvaluation;
+                TT.move = bestMove;
+                TT.depth = (sbyte)depth;
+                if (bestEvaluation < startingAlpha)
+                    TT.flag = 3; // Upperbound
+                else if (bestEvaluation >= beta)
+                {
+                    TT.flag = 2; // Lowerbound
+                    if (!bestMove.IsCapture) m_killerMoves[depth] = bestMove;
+                }
+                else
+                    TT.flag = 1; // Exact
+            }
+            
 
             return bestEvaluation;
         }
@@ -192,14 +204,12 @@ public class MyBot : IChessBot
                     int pieceValue = pieceValues[(int)piece.PieceType];
 
                     // If piece is opponent, make it negative
-                    if (piece.IsWhite != board.IsWhiteToMove) {
+                    if (piece.IsWhite != board.IsWhiteToMove)
                         pieceValue = -pieceValue;
-                        
-                    }
+        
                     totalEvaluation += pieceValue;
                 }
             }
-
             // Calculate mobility --> ELO diff increase of 100-130
             int myMobility = board.GetLegalMoves().Length;
             if (board.TrySkipTurn()) {
@@ -215,26 +225,67 @@ public class MyBot : IChessBot
         //--------------------------------------------------------------------------------
 
 
-        // Move ordering function
+        // Move ordering function --> significantly more tokens, but greatly improved efficiency!
         //--------------------------------------------------------------------------------
-        Move[] OrderMoves(Move[] moves)
+        void OrderMoves(ref Move[] moves, int depth)
         {
-            List<Move> captureMoves = new List<Move>();
-            List<Move> promotionMoves = new List<Move>();
-            List<Move> otherMoves = new List<Move>();
-
-            foreach (Move m in moves)
-            {
-                if (m.IsCapture) { 
-                    captureMoves.Add(m);
-                }
-                else if (m.IsPromotion) {
-                    promotionMoves.Add(m);
-                }
-                else otherMoves.Add(m);
-            }
-        
-            return captureMoves.Concat(promotionMoves).Concat(otherMoves).ToArray();
+            int[] movePriorities = new int[moves.Length];
+            for(int m = 0; m < moves.Length; m++) movePriorities[m] =  GetMovePriority(moves[m], depth);
+            Array.Sort(movePriorities, moves);
+            Array.Reverse(moves);
         }
+        //--------------------------------------------------------------------------------
+        int GetMovePriority(Move move, int depth)
+        {
+            int priority = 0;
+            // Check for a transposition table move (likely a PV move)
+            Transposition TT = m_TPTable[m_board.ZobristKey & 0x7FFFFF];
+            if(TT.move == move && TT.zobristHash == m_board.ZobristKey) 
+                priority += 100000;
+            
+            else if (move.IsCapture) // Order captures by MVV-LVA
+                priority =  1000 + 10 * (int)move.CapturePieceType - (int)move.MovePieceType;
+            
+            
+            else if (depth >= 0 && move.Equals(m_killerMoves[depth])) // Order killer moves
+                priority =  1;
+
+            return priority;
+        }
+        //--------------------------------------------------------------------------------
+
+
+        // Move ordering function [OLD]
+        //--------------------------------------------------------------------------------
+        // Move[] OrderMoves(Move[] moves)
+        // {
+        //     List<Move> captureMoves = new List<Move>();
+        //     List<Move> promotionMoves = new List<Move>();
+        //     List<Move> otherMoves = new List<Move>();
+
+        //     foreach (Move m in moves)
+        //     {
+        //         if (m.IsCapture) { 
+        //             captureMoves.Add(m);
+        //         }
+        //         else if (m.IsPromotion) {
+        //             promotionMoves.Add(m);
+        //         }
+        //         else otherMoves.Add(m);
+        //     }
+        
+        //     return captureMoves.Concat(promotionMoves).Concat(otherMoves).ToArray();
+        // }
+        //--------------------------------------------------------------------------------
+
+
+        // Depth timer
+        //--------------------------------------------------------------------------------
+        bool ShouldExecuteNextDepth(Timer timer, int maxThinkTime)
+        {
+            int currentThinkTime = timer.MillisecondsElapsedThisTurn;
+            return ((maxThinkTime - currentThinkTime) > currentThinkTime * 2);
+        }
+        //--------------------------------------------------------------------------------
     }
 }
